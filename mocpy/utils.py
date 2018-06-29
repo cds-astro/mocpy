f7623fa1423e2506dcc75f0ca10d1a2118faed39
Matthieu Baumann <matthieu.baumann@astro.unistra.fr>
2018-06-29 18:09:24 +0200

    better trailing zeroes algo

--- mocpy/utils.py
+++ mocpy/utils.py
@@ -17,6 +17,33 @@ def orderipix2uniq(n_order, n_pix):
     return n_pix + ((4**n_order) << 2)
 
 
+# x : int 64 bits
+def number_trailing_zeros(x):
+    bits = 0
+    if x == 0:
+        return 0
+
+    if not (x & 0xFFFFFFFF):
+        bits += 32
+        x >>= 32
+    if not (x & 0xFFFF):
+        bits += 16
+        x >>= 16
+    if not (x & 0xFF):
+        bits += 8
+        x >>= 8
+    if not (x & 0xF):
+        bits += 4
+        x >>= 4
+    if not (x & 0x3):
+        bits += 2
+        x >>= 2
+    if not (x & 1):
+        bits += 1
+        x >>= 1
+    return bits
+
+"""
 def number_trailing_zeros(i):
     # O(log(i)) complexity here. Can be done in O(1) using more low-level algo
     nb = 0
@@ -25,4 +52,4 @@ def number_trailing_zeros(i):
         i = i >> 1
 
     return nb
-
+"""
