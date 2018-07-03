9f08bc7761b1393e00ff7d6699e8a8ad5c6f4c24
Christoph Deil <Deil.Christoph@gmail.com>
2018-07-03 11:07:23 +0200

    Some docstring changes for better Sphinx docs

--- mocpy/__init__.py
+++ mocpy/__init__.py
@@ -1,9 +1,17 @@
 # Licensed under a 3-clause BSD style license - see LICENSE
 """
-MOCPy is a Python library allowing easy creation and manipulation of MOCs (Multi-Order Coverage maps).MOC is an `IVOA standard <http://ivoa.net/documents/MOC/>` enabling description of arbitrary sky regions. Based on the HEALPix sky tessellation, it maps regions on the sky into hierarchically grouped predefined cells.\nAn experimental support for TMOC (temporal MOC) has been added since version 0.4.0. It allows creation, parsing and comparison of TMOCs.
+MOCPy is a Python library allowing easy creation and manipulation of MOCs (Multi-Order Coverage maps).
+MOC is an `IVOA standard <http://ivoa.net/documents/MOC/>`__ enabling description of arbitrary sky regions.
+Based on the HEALPix sky tessellation, it maps regions on the sky into hierarchically grouped predefined cells.
+An experimental support for TMOC (temporal MOC) has been added since version 0.4.0.
+It allows creation, parsing and comparison of TMOCs.
 """
 
 from .moc import MOC
 from .tmoc import TimeMoc
 from .version import __version__
 
+__all__ = [
+    'MOC',
+    'TimeMoc',
+]
