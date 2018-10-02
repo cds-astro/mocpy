6f9c2aef4810dce404a1a03108beab5918b81ffa
Matthieu Baumann <matthieu.baumann@astro.unistra.fr>
2018-10-02 13:04:30 +0200

    restore setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(name='MOCPy',
       author_email='thomas.boch@astro.unistra.fr',
       license='BSD',
       url='https://github.com/cds-astro/mocpy',
-      requires=['astropy', 'astropy_healpix', 'numpy', 'matplotlib==2.2.2', 'networkx'],
+      requires=['astropy', 'astropy_healpix', 'numpy', 'matplotlib', 'networkx'],
       provides=['mocpy'],
       long_description="MOCPy is a Python library allowing easy creation and manipulation of MOCs (Multi-Order Coverage maps).MOC is an `IVOA standard <http://ivoa.net/documents/MOC/>` enabling description of arbitrary sky regions. Based on the HEALPix sky tessellation, it maps regions on the sky into hierarchically grouped predefined cells.\nAn experimental support for TMOC (temporal MOC) has been added since version 0.4.0. It allows creation, parsing and comparison of TMOCs.",
       classifiers=[
