d2b8a3403d73a56b8a60cadf75ef546feb5ca3f8
Matthieu Baumann <matthieu.baumann@astro.unistra.fr>
2019-06-05 13:49:32 +0200

    test stages travis and appveyor

--- travis/testing.sh
+++ travis/testing.sh
@@ -3,9 +3,9 @@
 # See https://stackoverflow.com/questions/821396/aborting-a-shell-script-if-any-command-returns-a-non-zero-value
 set -e
 
-python -m pytest -v mocpy --cov-report=term --cov=mocpy
+$PYTHON -m pytest -v mocpy --cov-report=term --cov=mocpy
 # Test notebooks
-pip install .
+$PIP install .
 # Install other dependencies for running the notebooks
-pip install jupyter astroquery
-python test_notebooks.py
+$PIP install jupyter astroquery
+$PYTHON test_notebooks.py
