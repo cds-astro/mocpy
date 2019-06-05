12c585e86fcf217ac959f992556b8b901cd0782c
Matthieu Baumann <matthieu.baumann@astro.unistra.fr>
2019-06-05 11:05:15 +0200

    test stages travis and appveyor

--- travis/testing.sh
+++ travis/testing.sh
@@ -3,11 +3,6 @@
 # See https://stackoverflow.com/questions/821396/aborting-a-shell-script-if-any-command-returns-a-non-zero-value
 set -e
 
-# Generate the dynamic library from the cdshealpix Rust crate.
-# This will download the crate from crates.io and build it first.
-python setup.py build_rust
-# Move the dynamic lib to the python package folder
-find build/ -name "*.so" -type f -exec cp {} ./mocpy \;
 python -m pytest -v mocpy --cov-report=term --cov=mocpy
 # Test notebooks
 pip install .
