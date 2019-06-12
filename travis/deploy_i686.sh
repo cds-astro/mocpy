482ed50299b18a53c1b6920c146d37f7614d5123
Matthieu Baumann <matthieu.baumann@astro.unistra.fr>
2019-06-12 14:08:55 +0200

    install freetype 2.4.0

--- travis/deploy_i686.sh
+++ travis/deploy_i686.sh
@@ -10,9 +10,12 @@ set -e
 
 ### Build the wheels ###
 $PIP install cibuildwheel==0.11.1 setuptools-rust
-export CIBW_BEFORE_BUILD="yum install -y freetype freetype-devel &&
- yum install -y libpng-devel &&
- yum install -y pkgconfig &&
+export CIBW_BEFORE_BUILD="yum install -y libpng-devel &&
+ yum install -y wget &&
+ wget https://download.savannah.gnu.org/releases/freetype/freetype-2.4.0.tar.gz &&
+ cd freetype-2.4.0 &&
+ make install &&
+ cd .. &&
  pip install setuptools-rust &&
  curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly -y"
 export CIBW_ENVIRONMENT='PATH="$HOME/.cargo/bin:$PATH"'
