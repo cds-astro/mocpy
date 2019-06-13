15090ea4427ab439820aa72c37214463b5a33903
Matthieu Baumann <matthieu.baumann@astro.unistra.fr>
2019-06-13 17:37:51 +0200

    checkout gh pages

--- travis/deploy_doc.sh
+++ travis/deploy_doc.sh
@@ -18,6 +18,7 @@ git remote add cds-astro https://"$GH_NAME":"$GH_TOKEN"@github.com/cds-astro/moc
 git checkout gh-pages
 rm -rf *
 
+
 git checkout cds-astro/master .
 
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly -y
