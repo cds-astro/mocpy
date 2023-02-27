6547116cbaaf390a8ebce0adbd7e6c77ed2573c0
ManonMarchand <manonmarchand22@gmail.com>
2023-02-27 17:09:18 +0100

    add function _set_wcs

--- python/mocpy/moc/plot/utils.py
+++ python/mocpy/moc/plot/utils.py
@@ -4,6 +4,23 @@ from astropy.wcs.utils import pixel_to_skycoord
 
 import warnings
 
+def _set_wcs(ax, wcs):
+    """Get the plot dimension the same way as in WCS constructor.
+    
+    This allows to pass an astropy wcs instead of a WCS when plotting.
+    """
+    #Inspired from issue: https://github.com/cds-astro/mocpy/issues/69 
+    width_px = wcs.wcs.crpix[0] * 2.0
+    height_px = wcs.wcs.crpix[1] * 2.0
+
+    x_min = 0
+    x_max = width_px - 1
+    y_min = 0
+    y_max = height_px - 1
+
+    ax.set_xlim([x_min, x_max])
+    ax.set_ylim([y_min, y_max])
+
 
 def build_plotting_moc(moc, wcs):
     """Plot a moc."""
@@ -58,5 +75,4 @@ def build_plotting_moc(moc, wcs):
 
     # The moc to plot is the INPUT_MOC & MOC_VIEWPORT. For small FOVs this can reduce
     # a lot the time to draw the MOC along with its borders.
-    moc_plot = moc_plot.intersection(moc_viewport)
-    return moc_plot
+    return moc_plot.intersection(moc_viewport)
