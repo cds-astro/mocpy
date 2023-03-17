65261cf7d2f849eb9de8146c762f3dc95a7ff57a
ManonMarchand <manonmarchand22@gmail.com>
2023-03-17 18:17:46 +0100

    add __version__ to __all__

--- python/mocpy/__init__.py
+++ python/mocpy/__init__.py
@@ -1,6 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE
 """
 MOCPy is a Python library allowing easy creation and manipulation of MOCs (Multi-Order Coverage maps).
+
 MOC is an `IVOA standard <http://ivoa.net/documents/MOC/>`__ enabling description of arbitrary sky regions.
 Based on the HEALPix sky tessellation, it maps regions on the sky into hierarchically grouped predefined cells.
 An experimental support for TMOC (temporal MOC) has been added since version 0.4.0.
@@ -18,4 +19,5 @@ __all__ = [
     "TimeMOC",
     "STMOC",
     "WCS",
+    "__version__",
 ]
