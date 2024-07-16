deabef1f96e1836ded52e39c6654116895d25b2d
ManonMarchand <ManonMarchand@users.noreply.github.com>
2024-07-16 12:19:57 +0000

    Deploying to gh-pages from @ cds-astro/mocpy@1a62e944ea71c5606dd568ad3c8bf9c77f0beeb0 🚀

--- _downloads/e7b30292640ba78467aae4181e63ff0b/logical_operations.py
+++ _downloads/e7b30292640ba78467aae4181e63ff0b/logical_operations.py
@@ -1,8 +1,7 @@
-import astropy.units as u
 import matplotlib.pyplot as plt
-from astropy.coordinates import Angle, SkyCoord
-
-from mocpy import MOC, WCS
+from astropy.visualization.wcsaxes.frame import EllipticalFrame
+from astropy.wcs import WCS
+from mocpy import MOC
 
 # Load Galex and SDSS
 sdss = MOC.from_fits("./../../resources/P-SDSS9-r.fits")
@@ -11,41 +10,46 @@ galex = MOC.from_fits("./../../resources/P-GALEXGR6-AIS-FUV.fits")
 inter = sdss & galex
 union = sdss + galex
 # Plot the MOC using matplotlib
-fig = plt.figure(111, figsize=(10, 10))
-# Define a astropy WCS easily
-with WCS(
-    fig,
-    fov=160 * u.deg,
-    center=SkyCoord(0, 0, unit="deg", frame="icrs"),
-    coordsys="icrs",
-    rotation=Angle(0, u.degree),
-    projection="AIT",
-) as wcs:
-    ax = fig.add_subplot(1, 1, 1, projection=wcs)
-    # Call fill with a matplotlib axe and the `~astropy.wcs.WCS` wcs object.
-    union.fill(
-        ax=ax,
-        wcs=wcs,
-        alpha=0.5,
-        fill=True,
-        color="red",
-        linewidth=0,
-        label="Union",
-    )
-    union.border(ax=ax, wcs=wcs, alpha=1, color="red")
+fig = plt.figure(111, figsize=(10, 8))
+# Define a astropy WCS
+wcs = WCS(
+    {
+        "naxis": 2,
+        "naxis1": 3240,
+        "naxis2": 1620,
+        "crpix1": 1620.5,
+        "crpix2": 810.5,
+        "cdelt1": -0.1,
+        "cdelt2": 0.1,
+        "ctype1": "RA---AIT",
+        "ctype2": "DEC--AIT",
+    },
+)
+ax = fig.add_subplot(1, 1, 1, projection=wcs, frame_class=EllipticalFrame)
+# Call fill with a matplotlib axe and the `~astropy.wcs.WCS` wcs object.
+union.fill(
+    ax=ax,
+    wcs=wcs,
+    alpha=0.5,
+    fill=True,
+    color="blue",
+    linewidth=0,
+    label="Union",
+)
+union.border(ax=ax, wcs=wcs, alpha=1, color="black")
 
-    inter.fill(
-        ax=ax,
-        wcs=wcs,
-        alpha=0.5,
-        fill=True,
-        color="green",
-        linewidth=0,
-        label="Intersection",
-    )
-    inter.border(ax=ax, wcs=wcs, alpha=1, color="green")
-    ax.legend()
-
+inter.fill(
+    ax=ax,
+    wcs=wcs,
+    alpha=0.5,
+    fill=True,
+    color="green",
+    linewidth=0,
+    label="Intersection",
+)
+inter.border(ax=ax, wcs=wcs, alpha=1, color="black")
+ax.legend()
+ax.set_aspect(1.0)
 plt.xlabel("ra")
 plt.ylabel("dec")
 plt.title("Logical operations between SDSS and GALEX")
