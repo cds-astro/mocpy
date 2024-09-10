29157fb6b1d81035de52621048af69769f8acd61
MARCHAND MANON <manonmarchand22@gmail.com>
2024-09-10 15:48:51 +0200

    feat: add option to turn off optimization

--- python/mocpy/moc/plot/utils.py
+++ python/mocpy/moc/plot/utils.py
@@ -22,20 +22,21 @@ def _set_wcs(ax, wcs):
     ax.set_ylim([y_min, y_max])
 
 
-def build_plotting_moc(moc, wcs):
+def build_plotting_moc(moc, wcs, optimize=True):
     """Plot a moc."""
-    # Get the WCS cdelt giving the deg.px^(-1) resolution.
-    cdelt = wcs.wcs.cdelt
-    # Convert in rad.px^(-1)
-    cdelt = np.abs((2 * np.pi / 360) * cdelt[0])
-    # Get the minimum depth such as the resolution of a cell is contained in 1px.
-    depth_res = int(np.floor(np.log2(np.sqrt(np.pi / 3) / cdelt)))
-    depth_res = max(depth_res, 0)
-    # Degrade the moc to that depth for plotting purposes. It is not necessary to plot pixels
-    # that we will not see because they are contained in 1px.
-    moc_plot = moc
-    if moc.max_order > depth_res:
-        moc_plot = moc.degrade_to_order(depth_res)
+    moc_plot = moc  # creates a copy to keep the original moc untouched
+    if optimize:
+        # Get the WCS cdelt giving the deg.px^(-1) resolution.
+        cdelt = wcs.wcs.cdelt
+        # Convert in rad.px^(-1)
+        cdelt = np.abs((2 * np.pi / 360) * cdelt[0])
+        # Get the minimum depth such as the resolution of a cell is contained in 1px.
+        depth_res = int(np.floor(np.log2(np.sqrt(np.pi / 3) / cdelt)))
+        depth_res = max(depth_res, 0)
+        # Degrade the moc to that depth for plotting purposes. It is not necessary to plot pixels
+        # that we will not see because they are contained in 1px.
+        if moc.max_order > depth_res:
+            moc_plot = moc.degrade_to_order(depth_res)
 
     # Get the MOC delimiting the FOV polygon
     width_px = int(wcs.wcs.crpix[0] * 2.0)  # Supposing the wcs is centered in the axis
@@ -56,7 +57,7 @@ def build_plotting_moc(moc, wcs):
     Y_px = np.append(Y_px, Y[-1, :-1])
     Y_px = np.append(Y_px, Y[1:, 0][::-1])
 
-    # Disable the output of warnings when encoutering NaNs.
+    # Disable the output of warnings when encountering NaNs.
     warnings.filterwarnings("ignore")
     # Inverse projection from pixel coordinate space to the world coordinate space
     viewport = pixel_to_skycoord(X_px, Y_px, wcs)
